96d62a2721a17539b05b8d046c8d41bb7bbc1132
usonia09 <uwasonia2001@gmail.com>
2024-03-04 23:56:36 -0500

    add assignments

---
+++ my-backend/src/routes/Assignments/+page.svelte
@@ -0,0 +1,17 @@
+<h1>Assigments</h1>
+<div class="Assigment">
+  <article>
+    <li>
+      <a
+        href="https://usonia09.github.io/Experimental_portifolio/Assignments/assignment2.html"
+        >Assigment 1: Exploratory Data Analysis</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://usonia09.github.io/Experimental_portifolio/Assignments/assignment3.html"
+        >Assigment 2: Visualization Design</a
+      >
+    </li>
+  </article>
+</div>
